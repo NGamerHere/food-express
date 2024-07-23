8ab7ddcefa12f33bc09e1436f8efa437f548f66c
NGamerHere <codingdatta@gmail.com>
2024-07-23 17:48:39 +0530

    writtien queries for the getting cities

--- src/routes/states.js
+++ src/routes/states.js
@@ -4,14 +4,16 @@ const pool = require("../db");
 const state = express.Router();
 
 
-state.get('/getCities',async (req, res) => {
+state.get('/api/getCities',async (req, res) => {
     try{
-        const [cities]= await pool.query('SELECT * FROM cities');
+        const data={};
+        const [cities]= await pool.query('select cities.id,cities.cityName,states.stateName from cities inner join states on states.id=cities.stateid');
         res.json(cities);
     }catch (e) {
         console.error('error in fetching cities '+e);
         res.json(e);
     }
 });
+
 
 module.exports=state;
