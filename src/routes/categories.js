3567a7a3f497aa09cd52ac952d1bbcae1c72568f
NGamerHere <codingdatta@gmail.com>
2024-07-24 14:32:45 +0530

    added the put and delete

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -28,4 +28,34 @@ categories.get('/api/categories',async (req,res)=>{
         res.status(500).json({'message':'internal server error',error:e});
     }
 })
+categories.patch('/api/categories',async (req,res)=>{
+    const userid=req.session.userId;
+    const id=req.body.id;
+    const categoryName=req.body.categoryName;
+    try{
+        const [result]=await pool.query('update categories set categoryName=? where id=? and restaurantID=?;',[categoryName,id,userid]);
+        if(result['affectedRows'] > 0 ){
+            res.json({'message':'done'});
+        }else{
+            res.status(500).json({'message':'error'});
+        }
+    }catch (e) {
+        console.log(e);
+        res.status(500).json({'message':'internal server error',error:e});
+    }
+});
+categories.delete('/api/categories',async (req,res)=>{
+   const id=req.body.id;
+   try{
+       const [result]=await pool.query('delete from categories where id=?',[id]);
+       if(result['affectedRows'] > 0 ){
+           res.json({'message':'done'});
+       }else {
+           res.status(500).json({'message':'error'});
+       }
+   }catch (e) {
+       console.log(e);
+       res.status(500).json({'message':'internal server error',error:e});
+   }
+});
 module.exports=categories;
