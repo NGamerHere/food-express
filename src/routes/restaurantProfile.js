844c580ea2572786297b2cf1492e017d5cddd5f5
NGamerHere <codingdatta@gmail.com>
2024-09-20 17:10:27 +0530

    added the little frontend code

--- src/routes/restaurantProfile.js
+++ src/routes/restaurantProfile.js
@@ -7,14 +7,21 @@ const restaurantProfile=express.Router();
 
 restaurantProfile.get('/location/:cityName',async (req,res)=>{
    const {cityName}=req.params;
-    const [resDetails]=
-        await pool.query(
-        'select restaurant.id,restaurantName,c.id,cityName from restaurant inner join foodexpress.cities as c on restaurant.cityID = c.id where c.cityName=?;', [cityName]);
+    //const [resDetails]=
+        //await pool.query(
+        //'select restaurant.id,restaurantName as name,c.id,cityName from restaurant inner join foodexpress.cities as c on restaurant.cityID = c.id where c.cityName=?;', [cityName]);
+
+    const [resDetails]=await pool.query('select * from restaurant');
+
     if(resDetails.length > 0){
-        res.send(resDetails);
+        const totalDetails=resDetails.map((item)=>{
+            item.description='this is the one of the wonderful city'
+            return item
+        })
+        res.render('restaurantsCity',{ restaurants: totalDetails , name: cityName })
     }else{
         //@todo need to add the city not found ejs rendering
-        res.send(cityName);
+        res.render('ResNotFound');
     }
 
 });
